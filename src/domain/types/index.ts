913321a2e849116ca39f12debb8d3ce0c55dcba3
nearKim <cheshire0814@gmail.com>
2025-12-25 14:18:51 -0500

    Remove legacy DDD architecture layers

--- src/domain/types/index.ts
+++
@@ -1,7 +0,0 @@
-export * as MessageEnvelope from './MessageEnvelope'
-export * as StatusMessage from './StatusMessage'
-export * as ErrorMessage from './ErrorMessage'
-
-export type { MessageEnvelope as MessageEnvelopeType, MessageEnvelopeParseError } from './MessageEnvelope'
-export type { StatusMessage as StatusMessageType } from './StatusMessage'
-export type { ErrorMessage as ErrorMessageType, ErrorCode } from './ErrorMessage'
